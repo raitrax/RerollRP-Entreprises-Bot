933e993f6b1292ea97f5b3efb6e38e6eb21b22c6
raitrax <maxime.maurer@viacesi.fr>
2024-05-15 19:24:19 +0200

    maj retrait api + ajout bouton astreinte

--- LSPD/src/events/ready.js
+++ LSPD/src/events/ready.js
@@ -2,60 +2,62 @@ const { Events, ActivityType, ActionRowBuilder, ButtonBuilder, ButtonStyle, Embe
 require('dotenv').config();
 const path = require('path');
 require('../scripts/deploy-commands')
-const { loadDashboard } = require(path.join(__dirname, '../api/main'));
 
 
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	async execute(client) {
-		let txtActivity;
-		const guild = client.guilds.cache.get(process.env.GUILD_ID); // puppets server id
-		await guild.members.fetch();
-		const countPDS = guild.roles.cache.get(process.env.SERVICE_ROLE_ID).members;
-		if (countPDS != null) {
-			txtActivity = `${countPDS.size} personne(s) en service`
-		}
-		else {
-			txtActivity = `0 personne(s) en service`
-		}
-		console.log(txtActivity);
-		client.user.setActivity({ name: txtActivity, type: ActivityType.Custom });
+  name: Events.ClientReady,
+  once: true,
+  async execute(client) {
+    let txtActivity;
+    const guild = client.guilds.cache.get(process.env.GUILD_ID); // puppets server id
+    await guild.members.fetch();
+    const countPDS = guild.roles.cache.get(process.env.SERVICE_ROLE_ID).members;
+    if (countPDS != null) {
+      txtActivity = `${countPDS.size} personne(s) en service`
+    }
+    else {
+      txtActivity = `0 personne(s) en service`
+    }
+    console.log(txtActivity);
+    client.user.setActivity({ name: txtActivity, type: ActivityType.Custom });
 
-		const channel = client.channels.cache.get(process.env.PDS_CHANNEL_ID);
-		/*await channel.bulkDelete(99, true).catch(error => {
-			console.error(error);
-			channel.send({ content: 'Une erreur est survenue lors de la suppression des messages dans ce channel!', ephemeral: true });
-		});*/
-		await channel.send({ embeds: [getServiceRequestEmbed(channel)], components: [getServiceRequestButtons()] });
+    const channel = client.channels.cache.get(process.env.PDS_CHANNEL_ID);
+    /*await channel.bulkDelete(99, true).catch(error => {
+      console.error(error);
+      channel.send({ content: 'Une erreur est survenue lors de la suppression des messages dans ce channel!', ephemeral: true });
+    });*/
+    await channel.send({ embeds: [getServiceRequestEmbed(channel)], components: [getServiceRequestButtons()] });
 
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-		loadDashboard(client);
-	},
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+  },
 };
 
 const getServiceRequestEmbed = (channel) => {
 
-	return new EmbedBuilder()
-		.setColor(Colors.Green)
-		.setDescription(`Pour indiquer une prise / fin de service - Appuyer sur le bouton 🔴\n Pour prendre la centrale - Appuyez sur 🟢`)
-		//.setDescription('Bon jeu!')
-		.setAuthor({ name: 'Centrale LSPD' })
-		.setTimestamp(new Date());
+  return new EmbedBuilder()
+    .setColor(Colors.Green)
+    .setDescription(`Pour indiquer une prise / fin de service - Appuyer sur le bouton 🔴\n Pour prendre la centrale - Appuyez sur 🟢\n Pour se mettre en astreinte - Appuyez sur ⚫`)
+    //.setDescription('Bon jeu!')
+    .setAuthor({ name: 'Centrale LSPD' })
+    .setTimestamp(new Date());
 
 };
 
 const getServiceRequestButtons = () => {
-	return new ActionRowBuilder()
-		.addComponents(
-			new ButtonBuilder()
-				.setCustomId('service')
-				.setLabel('Service')
-				.setStyle(ButtonStyle.Danger),
-			new ButtonBuilder()
-				.setCustomId('lead')
-				.setLabel('Lead')
-				.setStyle(ButtonStyle.Success)
-		);
+  return new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+        .setCustomId('service')
+        .setLabel('Service')
+        .setStyle(ButtonStyle.Danger),
+      new ButtonBuilder()
+        .setCustomId('lead')
+        .setLabel('Lead')
+        .setStyle(ButtonStyle.Success),
+      new ButtonBuilder()
+        .setCustomId('astreinte')
+        .setLabel('Astreinte')
+        .setStyle(ButtonStyle.Secondary)
+    );
 };
 
