51f30f53bd87fd53ceba48686a59365ef30c378e
raitrax <maxime.maurer@viacesi.fr>
2024-05-16 21:07:38 +0200

    création de fil à l'ajout d'un agent LSPD

--- LSPD/index.js
+++ LSPD/index.js
@@ -4,16 +4,16 @@ const { Client, Collection, GatewayIntentBits, DiscordAPIError, Partials } = req
 require('dotenv').config();
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.MessageContent,
-	],
-	partials: [
-		Partials.Channel,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [
+    Partials.Channel,
+  ],
 });
 
 client.commands = new Collection();
@@ -28,49 +28,49 @@ const cronsPath = path.join(__dirname, 'src', 'crons');
 const cronsFiles = fs.readdirSync(cronsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	console.log('commands:', command.data.name);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	}
-	else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  console.log('commands:', command.data.name);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  }
+  else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
 }
 
 for (const file of eventsFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	console.log('events:', event.name);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	}
-	else {
-		client.on(event.name, (...args) => {
-			try {
-				event.execute(...args);
-			}
-			catch (error) {
-				if (error instanceof DiscordAPIError && error.code === 10062) {
-					console.error('Interaction inconnue: ', error.url);
-				}
-				else {
-					console.error(error);
-				}
-			}
-		});
-	}
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  console.log('events:', event.name);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  }
+  else {
+    client.on(event.name, (...args) => {
+      try {
+        event.execute(...args);
+      }
+      catch (error) {
+        if (error instanceof DiscordAPIError && error.code === 10062) {
+          console.error('Interaction inconnue: ', error.url);
+        }
+        else {
+          console.error(error);
+        }
+      }
+    });
+  }
 }
 
 client.login(process.env.DISCORD_TOKEN).then(() => {
-	// client.user.setActivity({ name: '👀', type: ActivityType.Custom });
+  // client.user.setActivity({ name: '👀', type: ActivityType.Custom });
 
-	for (const file of cronsFiles) {
-		const filePath = path.join(cronsPath, file);
-		const cron = require(filePath);
-		console.log('cronFile:', file);
-		cron.initCron(client);
-	}
+  for (const file of cronsFiles) {
+    const filePath = path.join(cronsPath, file);
+    const cron = require(filePath);
+    console.log('cronFile:', file);
+    cron.initCron(client);
+  }
 });
--- LSPD/src/events/guildMemberAdd.js
+++ LSPD/src/events/guildMemberAdd.js
@@ -1,35 +1,52 @@
-const { Events } = require('discord.js');
+const { Events, ThreadAutoArchiveDuration } = require('discord.js');
 require('dotenv').config();
 
 module.exports = {
-	name: Events.GuildMemberAdd,
-	async execute(member) {
-		console.warn(`New Member Joined --- id: ${member.id}\n--- name: ${member.displayName}`);
-		//check si bien embauché LSPD
-		// if a remplacer par le check metier joueur via api
-		if (member.roles.cache.has(process.env.LSPD_ROLE_ID)) {
-			try {
-				member = await member.roles.add(process.env.LSPD_ROLE_ID);
-				member = await member.roles.add(process.env.CADET_ROLE_ID);
-			}
-			catch (err) {
-				console.error(err);
-				return;
-			}
-		}
-		// if a remplacer par le check metier joueur via api
-		if (member.roles.cache.has(process.env.SAMS_ROLE_ID)) {
-			try {
-				member = await member.roles.add(process.env.SAMS_ROLE_ID);
-			}
-			catch (err) {
-				console.error(err);
-				return;
-			}
-		}
-		//cas joueur non traité par les exeptions
-		console.log("joueur non répertorié dans une entreprise");
-		//cas utilisateur non joueur ajouté au serveur
-		console.log("cette personne n'est pas un joueur");
-	},
+  name: Events.GuildMemberAdd,
+  async execute(member) {
+    console.warn(`New Member Joined --- id: ${member.id}\n--- name: ${member.displayName}`);
+    const headers = {
+      Authorization: `Bearer ${process.env.API_REROLL_TOKEN}`,
+    };
+    let response;
+    try {
+      response = await fetch(`https://api.reroll-rp.fr/v2/character/discord/${member.id}`, {
+        headers,
+      });
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+    const character = await response.json();
+    if (character?.error?.code === 404) {
+      console.error(`recruit fetchData - Joueur <@${member.id}> non trouvé!`);
+      return false;
+    }
+
+    if (character.job.id === 'lspd') {
+      const channel = member.guild.channels.cache.find((channel) => channel.id === process.env.FORMATION_CHANNEL_ID);
+      const thread = await channel.threads.create({
+        name: `00 - ${character.charinfo.firstname} ${character.charinfo.lastname}`,
+        autoArchiveDuration: ThreadAutoArchiveDuration.OneWeek,
+        reason: 'Fiche Cadet',
+      });
+      thread.send('```Permis voiture```');
+      thread.send('```Permis moto```');
+      thread.send('```Permis PL```');
+      thread.send('```Visite médicale```');
+      thread.send('```Accueil et information de base```');
+      thread.send('```Matériel obligatoire```');
+      thread.send('```PPA```');
+      thread.send('```Habilitation FAP```');
+      thread.send('```Habilitation MP5```');
+      thread.send('```Utilisation radio et code 10```');
+      thread.send("```Base d'un contrôle```");
+      thread.send('```Conduite```');
+      thread.send('```Barrage routier```');
+      thread.send('```Mise en cellule```');
+      thread.send('```Formation Pit```');
+      thread.send('```Simulation lincoln```');
+      console.log(`Created thread: ${thread.name}`);
+    }
+  },
 };
